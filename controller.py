6bafd33a4b7f4f4727c031cd83b3b0877b107dac
ThomasMousseau <univers220@gmail.com>
2023-08-11 08:38:21 -0400

    Health check before deploy

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ import uvicorn
 import pickle
 from privateGPT import getAnswer 
 
-app = FastAPI()
+app = FastAPI() #root should be /api/
 
 @app.get('/question/{prompt}')
 async def get_data(prompt: str):
@@ -14,6 +14,11 @@ async def get_data(prompt: str):
         return getAnswer(prompt), 200
     except Exception as e:
         return str(e), 500
+
+@app.get('/health')
+async def health():
+    return 'OK', 200
+    
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
